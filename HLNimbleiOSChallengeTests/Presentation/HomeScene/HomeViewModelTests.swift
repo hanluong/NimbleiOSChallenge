75620f53dd799b23cac67ca862ec8af97c80f0b5
hanluong <phr.luonghan.bb@gmail.com>
2023-10-25 12:17:57 +0700

    Prepare for home and login model tests

---
+++ HLNimbleiOSChallengeTests/Presentation/HomeScene/HomeViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModelTests.swift
+//  HLNimbleiOSChallengeTests
+//
+//  Created by Han Luong on 25/10/2023.
+//
+
+import XCTest
+@testable import HLNimbleiOSChallenge
+
+class HomeViewModelTests: XCTestCase {
+    
+}
---
+++ HLNimbleiOSChallengeTests/Presentation/LoginScene/LoginViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModelTests.swift
+//  HLNimbleiOSChallengeTests
+//
+//  Created by Han Luong on 25/10/2023.
+//
+
+import XCTest
+@testable import HLNimbleiOSChallenge
+
+class LoginViewModelTests: XCTestCase {
+    
+    func testLoginWithCorrectDetails() throws {
+    }
+    
+}
