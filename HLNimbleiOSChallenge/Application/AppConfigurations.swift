86729293edc02fd7ab8fc91fe86f33536d14747d
hanluong <phr.luonghan.bb@gmail.com>
2023-10-31 09:49:17 +0700

    Support multiples app environments (Debug, Staging, Release)

--- HLNimbleiOSChallenge/Application/AppConfigurations.swift
+++ HLNimbleiOSChallenge/Application/AppConfigurations.swift
@@ -9,22 +9,22 @@ import Foundation
 
 final class AppConfigurations {
     lazy var rootUrl: String = {
-        guard let url = Bundle.main.object(forInfoDictionaryKey: "ROOT_URL") as? String else {
-            fatalError("ROOT_URL must not be empty in plist")
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else {
+            fatalError("BASE_URL must not be empty in plist")
         }
-        return url
+        return url.replacingOccurrences(of: "\\", with: "")
     }()
     
     lazy var clientId: String = {
-        guard let id = Bundle.main.object(forInfoDictionaryKey: "CLIENT_ID") as? String else {
-            fatalError("CLIENT_ID must not be empty in plist")
+        guard let id = Bundle.main.object(forInfoDictionaryKey: "NIMBLE_CLIENT_ID") as? String else {
+            fatalError("NIMBLE_CLIENT_ID must not be empty in plist")
         }
         return id
     }()
     
     lazy var clientSecret: String = {
-        guard let secretKey = Bundle.main.object(forInfoDictionaryKey: "CLIENT_SECRET") as? String else {
-            fatalError("CLIENT_SECRET must not be empty in plist")
+        guard let secretKey = Bundle.main.object(forInfoDictionaryKey: "NIMBLE_CLIENT_SECRET") as? String else {
+            fatalError("NIMBLE_CLIENT_SECRET must not be empty in plist")
         }
         return secretKey
     }()
